03f89e1aeb5cb22d46e8762bc2bd132863fd6580
Antonius Sakti Wiradinata <wira@xashlez.com>
2024-11-10 20:21:10 +0700

    [UPDATE] change env

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
+import os
+from dotenv import load_dotenv
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://wiradinataa9:wdinata33@data.etzoacx.mongodb.net/')
-db = client['chatbot']
-products_collection = db['products']
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the MongoDB URI from environment variables
+MONGO_URI = os.getenv('MONGO_URI')
+
+# Set up MongoDB client
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = client['chatbot']  # Specify your database name
+products_collection = db['products']  # Specify your collection name
 
 async def get_all_products():
-    # Retrieve all products from the collection
+    """Retrieve all products from the collection."""
     products = await products_collection.find().to_list(length=None)
     return products
