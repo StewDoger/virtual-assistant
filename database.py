f97b2ebb9789e142547e14654b6fc0bcf39ebe42
Antonius Sakti Wiradinata <wira@xashlez.com>
2024-11-11 03:51:16 +0700

    [UPDATE] change db

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
-import motor.motor_asyncio
-from pymongo import MongoClient
+import motor.motor_asyncio  # Pastikan menggunakan motor untuk async MongoDB
 
 # Load environment variables from .env file
 load_dotenv()
@@ -9,14 +8,15 @@ load_dotenv()
 # Retrieve the MongoDB URI from environment variables
 mongo_uri = os.getenv('MONGO_URI')
 
-# Set up MongoDB client
-client = MongoClient(mongo_uri)
+# Set up MongoDB async client (Motor)
+client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
 db = client.chatbot
 products_collection = db['products']
 
 async def get_all_products():
-    """Retrieve all products from the collection."""
+    """Retrieve all products from the collection asynchronously."""
     try:
+        # Menggunakan to_list() untuk hasil query asinkron
         products = await products_collection.find().to_list(length=None)
         return products
     except Exception as e:
